cac7af080d0ea1267dfe6bc3c7db997be0bebd38
Gridcoder <gridcoder@users.noreply.github.com>
2025-01-30 11:07:27 -0400

    2025-01-30@10:54 - Updated FormattedDate component with property to include a period. - Updated Footer component styling and text. - Updated Footer translations.

--- src/locales/locales.d.ts
+++ src/locales/locales.d.ts
@@ -4,7 +4,9 @@ declare interface Locales {
 	about: string;
 	blog: string;
 	"nav-mobile-menu": string;
-	footer: string;
+	footer1: string;
+	footer2: string;
+	footer3: string;
 	"404": string;
 	"404-message": string;
 	"404-back": string;
