6c5f5840cf4b2e8fe1298e60001ea7f9bae11b1c
Gridcoder <gridcoder@users.noreply.github.com>
2025-01-30 11:34:02 -0400

    2025-01-30@11:32 - Updated Footer. - Removed About link from navigation.

--- src/consts.ts
+++ src/consts.ts
@@ -83,10 +83,10 @@ export const PAGES: Page[] = [
 	// 	title: "blog",
 	// 	href: "/blog",
 	// },
-	{
-		title: "about",
-		href: "/about",
-	},
+	// {
+	// 	title: "about",
+	// 	href: "/about",
+	// },
 ];
 
 // i18n
